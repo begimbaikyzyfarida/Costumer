499cf9e7ac7aa4193cee195d61462b91a27b7ad1
FARIDA <140826748+begimbaikyzyfarida@users.noreply.github.com>
2023-08-14 17:41:15 +0600

    Create Bank.java

---
+++ Bank.java
@@ -0,0 +1,44 @@
+public class Bank {
+    private String userName;
+    private String password;
+    private int totalMoney;
+
+    public Bank(String userName, String password, int totalMoney) {
+        this.userName = userName;
+        this.password = password;
+        this.totalMoney = totalMoney;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTotalMoney() {
+        return totalMoney;
+    }
+
+    public void setTotalMoney(int totalMoney) {
+        this.totalMoney = totalMoney;
+    }
+
+    @Override
+    public String toString() {
+        return "Bank{" +
+                "userName='" + userName + '\'' +
+                ", password=" + password +
+                ", totalMoney=" + totalMoney +
+                '}';
+    }
+}
